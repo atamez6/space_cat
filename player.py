f0d309cd1ccd5099f68085fd94b9dfeee2a47bd6
AT <at@ATs-MacBook-Pro.local>
2025-01-09 15:47:57 -0600

    movimiento player

--- player.py
+++ player.py
@@ -8,12 +8,22 @@ class Player(pygame.sprite.Sprite):
         image_resize = (80, 80)  # Cambiar al tamaño deseado
         self.image = pygame.transform.scale(self.image, image_resize)
 
-        #self.image.set_colorkey((255, 255, 255))  # Blanco
-        #self.image.set_colorkey((0, 0, 0))  # Negro
-
         self.rect = self.image.get_rect()
         self.rect.center = (settings.WIDTH // 2, settings.HEIGHT // 2)  # Centro de la pantalla
 
-        #self.rect.centerx = settings.WIDTH // 2
         self.rect.bottom = settings.HEIGHT - 10
         self.speed_axe_x = 0
+
+    def update(self):
+        self.speed_axe_x = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speed_axe_x = -6
+        if keystate[pygame.K_RIGHT]:
+            self.speed_axe_x = 5
+        self.rect.x += self.speed_axe_x
+
+        if self.rect.right > settings.WIDTH:
+            self.rect.right = settings.WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
