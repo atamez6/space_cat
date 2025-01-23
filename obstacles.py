03c1927de21883d89ad81eefcb152e8769dbf91c
Your Name <you@example.com>
2025-01-23 17:23:57 -0600

    banana icon

--- obstacles.py
+++ obstacles.py
@@ -1,11 +1,21 @@
 
-import pygame
 import settings
 
+import pygame
+
 class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, x, y, width=50, height=50):
+    def __init__(self, x, y, image_path="assets/images/avatars/planeta.png"):
         super().__init__()
-        self.image = pygame.image.load("assets/images/avatars/banana.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (width, height))
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (100, 100))  # Tamaño ajustado
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.health = 100  # Vida del obstáculo
+
+    def take_damage(self):
+        self.health -= 50  # Reduce la vida
+        if self.health <= 0:
+            self.kill()  # Elimina el obstáculo si su vida es 0 o menos
+
+    def update(self):
+        pass  # Los obstáculos no tienen movimiento
